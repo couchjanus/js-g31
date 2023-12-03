27a194f60b975462aa0f6dc016dbaadcd563bbe0
couchjanus <couchjanus@gmail.com>
2023-12-03 12:57:34 +0200

    added unit 3

--- js/main.js
+++ js/main.js
@@ -1,81 +1,15 @@
 // commnt
 'use strict';
 
-/**
- * comments
- */
-// console.dir(document.body.innerHTML)
-// console.dir(document.title)
-// console.dir(document.head)
-
-// console.dir(document.body.nav)
-// console.dir(document.body.navbar)
-
-// let nav = document.getElementsByTagName('nav');
-// console.dir(nav);
-
-// let navbar = document.getElementById('navbar');
-// console.dir(navbar);
-
-// let navbars = document.getElementsByClassName('navbar')
-// console.dir(navbars);
-
-// let input = document.getElementsByName('inp')
-// console.log(input)
-
-// let div = document.querySelector('div')
-// console.dir(div);
-
-// let divs = document.querySelectorAll('div')
-// console.dir(divs);
-
-// let catalog = document.querySelectorAll('.catalog');
-// console.dir(catalog)
-
-// let catalog1 = document.querySelectorAll('#catalog');
-// console.dir(catalog1)
-
-// console.log(divs[80])
-
-// for (let i=0; i<10; i++) {
-//     console.log(divs[i])
-// }
-
-
-// for (let i=0; i<divs.length; i++) {
-//     console.log(divs[i])
-// }
-
 const  totalAmoutInWishlist = document.getElementById("total-amout-in-wishlist");
-console.dir(totalAmoutInWishlist)
-
-// totalAmoutInWishlist.style.color = "red"
-
-// totalAmoutInWishlist.innerText = 55
-
 const  totalAmoutInCart = document.getElementById("total-amout-in-cart");
-console.dir(totalAmoutInCart)
+
 
 let addToCartButtons = document.querySelectorAll('.add-to-cart');
-console.dir(addToCartButtons);
+
 
 let addToWishlistButtons = document.querySelectorAll('.add-to-wishlist');
-console.dir(addToWishlistButtons);
 
-// addToCartButtons.forEach(function(button){
-//     console.dir(button);
-// });
-
-// function counter() {
-//     let value = +totalAmoutInWishlist.innerText; 
-//     // parseInt()
-//     return function(increment) {
-//         value += increment;
-//         totalAmoutInWishlist.innerText = value;
-//         totalAmoutInWishlist.style.color = "red";
-//     }
-// }
-// let totalInWishlist = counter();
 
 function counter(total) {
     let value = +total.innerText; 
@@ -90,51 +24,194 @@ function counter(total) {
 let totalInWishlist = counter(totalAmoutInWishlist);
 let totalInCart = counter(totalAmoutInCart);
 
-let products = document.querySelectorAll('.product');
-console.dir("products = ", products);
+let productItemTemplate = product => `
+<div class="product" data-id="${product.id}">
+            <div class="icons">
+              <a href="#" class="fas fa-shopping-cart add-to-cart"></a>
+              <a href="#" class="fas fa-heart add-to-wishlist"></a>
+              <a href="#productView" class="fas fa-eye detail"></a>
+            </div>
+            <div class="image">
+              <div class="badge text-white bg-${product.badge.bg}">${product.badge.title}</div>
+              <img src="${product.image}">
+            </div>
 
-if (products.length >= 1) {
+            <div class="content">
+              <h3>${product.name}</h3>
+              <div class="price">${product.price}</div>
+            </div>
+</div>
+`;
 
-console.dir(products);
+const productContainer = document.querySelector('.product-container');
 
-products.forEach(function(product){
-    // console.log(product.querySelector('h3').innerText);
-    // console.log(product.querySelector('.price').innerText);
-    // console.log(product.querySelector('.add-to-cart'));
-    product.querySelector('.add-to-cart').addEventListener('click', function() {
-        console.log(product.querySelector('h3').innerText);
-        console.log(product.querySelector('.price').innerText);
-        totalInCart(1);
-    })
-});
-
-// products.forEach(function(product){
-    
-//     product.querySelector('.add-to-wishlist').addEventListener('click', function() {
-//         console.log(product.querySelector('h3').innerText);
-//         console.log(product.querySelector('.price').innerText);
-//         totalInWishlist(1);
-//     })
-// });
-
-products.forEach(product => {
-    
-    product.querySelector('.add-to-wishlist').addEventListener('click', () => {
-        console.log(product.querySelector('h3').innerText);
-        console.log(product.querySelector('.price').innerText);
-        totalInWishlist(1);
-    });
-});
+function populateProductList(products) {
+    let result = '';
+    for (const item of products) {
+        result += productItemTemplate(item);
+    }
+    return result;
 }
-// console.log(parseInt(totalAmoutInWishlist.innerText))
+
+class Store 
+{
+    static init(key) {
+        if(!Store.isset(key)) {
+            Store.set(key, []); 
+        }
+        return Store.get(key);
+    } 
+    static set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    static get(key) {
+        let value = localStorage.getItem(key);
+        return value === null ? null : JSON.parse(value);
+    }
+
+    static isset(key) {
+        return this.get(key) !== null;
+    }
+}
+
+let cart = [];
+let wishlist = [];
+
+const findItem = (items, id) => items.find(item => item.id == id);
+
+function addProductToCart(product, amount=1){
+
+    let inCart = cart.some(item => item.id === product.id);
+
+    if (inCart) {
+        cart.forEach(item => {
+            if(item.id === product.id) {
+                item.amount += amount;
+            }
+        })
+    }else {
+        let cartItem = {...product, amount: amount};
+        cart = [...cart, cartItem];
+    }
+
+    Store.set('basket', cart);
+}
+
+function addProductToCartButton() {
+    let addToCartButtons = document.querySelectorAll('.add-to-cart');
+    addToCartButtons.forEach(item => {
+        item.addEventListener('click', (event) => {
+            // console.log(item, event)
+            let id = event.target.closest('.product').dataset.id;
+            // console.log(id)
+            addProductToCart({id: id});
+        });
+    });
+    
+}
+
+
+const modalTemplate = (product) => `
+<div class="modal" id="productView">
+  <div class="modal-dialog">
+    <a href="#!" title="Close" class="close fas fa-times"></a>
+    <div class="modal-body">
+
+      <aside><img src="${product.image}"></aside>
+
+      <main>
+        <div class="info-container">
+
+          <div class="info-header"><h2>${product.name}</h2></div>
+
+          <div class="info-price">${product.price}</div>
+          <div class="info-shipping">Free shipping</div>
+          
+          <div class="info-button">
+            <a href="#!" class="btn btn-submit"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+          </div>
+
+          <div class="qty info-qty">
+           <button class="btn btn-dec">-</button>
+            
+            <input 
+                    type="number" 
+                    value="1"
+                    min="1"
+                    max="10"
+                    required 
+                    />
+                    <button class="btn btn-inc">+</button>
+          </div>
+
+          <div class="info-description">
+          ${product.description} 
+          </div>
+
+        </div>
+      </main>
+
+    </div>
+  </div>
+</div>
+`;
+
+function renderModal(modalWindow, cart) {
+    modalWindow.querySelector('.btn-inc').addEventListener('click', e => {
+        let val = e.target.previousElementSibling.value;
+        val++;
+        e.target.previousElementSibling.value = val;
+    })
+    modalWindow.querySelector('.btn-dec').addEventListener('click', e => {
+        let val = e.target.nextElementSibling.value;
+        val--;
+        e.target.nextElementSibling.value = val;
+    })
+}
+
+function toggleModal(modalWindow, cart, param, product={}) {
+    if (modalWindow.innerHTML=='') {
+        modalWindow.innerHTML = modalTemplate(product);
+        renderModal(modalWindow, cart)
+    }else {
+        modalWindow.innerHTML='';
+    }
+    modalWindow.style.display = param;
+}
+
+function detailButton() {
+    const modalWindow = document.querySelector('.modal-window');
+    let detailButtons = document.querySelectorAll('.detail');
+
+    detailButtons.forEach(item => {
+        item.addEventListener('click', event => {
+            let id = event.target.closest('.product').dataset.id;
+            let product = findItem(products, id);
+            toggleModal(modalWindow, cart, 'block', product);
+
+            modalWindow.querySelector('.close').addEventListener('click', event => {
+                toggleModal(modalWindow, cart, 'none');
+                
+            })
+
+        })
+    })
+
+}
 
 function main() {
-    console.log("DOM loaded")
+
+    cart = Store.init('basket');
+    wishlist = Store.init('wishlist');
+
+    productContainer.innerHTML = populateProductList(products);
+
+    addProductToCartButton();
+    detailButton();
 }
 
-document.addEventListener("DOMContentLoaded", () => {
 
-});
 
 if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", main)
